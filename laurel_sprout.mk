0334e1767783e21fa6688b9b0839a8cd80656163
Musical143143 <basub4615@gmail.com>
2023-02-22 20:03:44 +0100

    laurel_sprout:Support udfps on AOD

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -465,6 +465,12 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
 
+PRODUCT_PACKAGES += \
+    sensors.udfps
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Shim
 PRODUCT_PACKAGES += \
     libims_shim \
